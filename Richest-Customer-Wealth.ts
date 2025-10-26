97cecb224c9d5c203b28800b6995d911d0e119d4
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-10-26 21:20:33 +0100

    Add files via upload

---
+++ Richest-Customer-Wealth.ts
@@ -0,0 +1,20 @@
+// 1672. Richest Customer Wealth
+// Easy | Array
+
+function maximumWealth(accounts: number[][]): number {
+    let Biggest_total: number = 0
+    for (let i = 0; i<accounts.length; i++){
+        let current_total : number = 0;
+        for (let j = 0; j < accounts[i].length; j++) {
+            current_total += accounts[i][j]
+            if(Biggest_total<current_total){
+                Biggest_total = current_total
+            };
+        };
+    };
+    return Biggest_total
+};
+
+console.log(maximumWealth([[1,2,3],[3,2,1]]));
+console.log(maximumWealth([[1,5],[7,3],[3,5]]));
+console.log(maximumWealth([[2,8,7],[1,9,5]]));

f4c8465162e689053464b11abb6b4cf3a264d941
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-10-26 21:25:52 +0100

    Format and clean up Richest-Customer-Wealth.ts

--- Richest-Customer-Wealth.ts
+++ Richest-Customer-Wealth.ts
@@ -18,5 +18,5 @@ function maximumWealth(accounts: number[][]): number {
 
 console.log(maximumWealth([[1,2,3],[3,2,1]]));
 console.log(maximumWealth([[1,5],[7,3],[3,5]]));
-
 console.log(maximumWealth([[2,8,7],[1,9,5]]));
+

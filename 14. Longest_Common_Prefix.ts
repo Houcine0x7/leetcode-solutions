6d80c9172ec482119494ba0a1b12c01782bd35c1
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-11-02 23:52:52 +0100

    Add files via upload

---
+++ 14. Longest_Common_Prefix.ts
@@ -0,0 +1,21 @@
+// Typescript
+// 14. Longest Common Prefix
+// Easy | Arry
+
+
+function longestCommonPrefix(strs: string[]): string {
+    let prefix: string = ""
+    for (let i = 0; i<strs[0].length; i++){
+        for(let j = 0; j<strs.length; j++){
+            if(strs[j][i] !== strs[0][i]){
+              return  prefix
+            }  
+        }
+        prefix+=strs[0][i]
+    }
+    return prefix
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
+

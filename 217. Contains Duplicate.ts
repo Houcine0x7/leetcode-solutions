cc89002e6ae77d5aad76ee33f140295597384562
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-12-21 19:30:45 +0100

    Add files via upload

---
+++ 217. Contains Duplicate.ts
@@ -0,0 +1,20 @@
+// Typescript
+// 217. Contains Duplicate
+// Easy | Array & Hash Set
+// Time | complexity : O(n)
+// Space | complexity : O(n)
+
+function containsDuplicate(nums: number[]): boolean {
+    let numberSet = new Set<number>();
+    for (const num of nums){
+        if(numberSet.has(num)){
+            return true;
+        }
+        numberSet.add(num);
+    }
+    return false
+}
+
+console.log(containsDuplicate([1,2,3,1])) // Output: true
+console.log(containsDuplicate([1,2,3,4])) // Output: false
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])) // Output: true

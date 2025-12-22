8f9253547a86f3421a4f46ed013972ed4144f8e0
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-12-22 23:05:32 +0100

    Add files via upload

---
+++ 1. Two Sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map: Map<number, number> = new Map<number, number>()
+    for (let i = 0; i < nums.length; i++) {
+        let complement: number = target - nums[i]
+        if (map.has(complement)){
+            return [map.get(complement)!, i]
+        }
+        map.set(nums[i], i)
+    }
+    return [];
+};
+console.log(twoSum([2,7,11,15], 9)) // Output: 
+console.log(twoSum([3,2,4], 6)) // Output: 
+console.log(twoSum([3,3], 6)) // Output: 

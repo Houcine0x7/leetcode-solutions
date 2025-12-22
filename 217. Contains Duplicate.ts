5683d5693308233fdf0339d843024c5aea894435
Haddadi Houcine <121579234+Houcine0x7@users.noreply.github.com>
2025-12-22 22:55:40 +0100

    Reformat code with consistent comment style

--- 217. Contains Duplicate.ts
+++ 217. Contains Duplicate.ts
@@ -17,5 +17,5 @@ function containsDuplicate(nums: number[]): boolean {
 
 console.log(containsDuplicate([1,2,3,1])) // Output: true
 console.log(containsDuplicate([1,2,3,4])) // Output: false
-
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])) // Output: true
+
